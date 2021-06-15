b09c9941184add3ac47b7c4783501212714f62d0
Maeda, Itaru <developer@adeams.com>
2021-06-15 22:37:06 +0900

    http://api.weatherapi.com から気象データを取得する部分を追加

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,11 +4,16 @@ import { useState } from 'react';
 
 const Form = () => {
   const [city, setCity] = useState<string>("");
+  const getWeather = (e: any) => {
+    e.preventDefault();
+    fetch("https://api.weatherapi.com/v1/current.json?key=db428fe31ef04bc99f9131340211506&q=London&aqi=no")
+          .then(res => res.json())
+          .then(data => console.log(data))
+  }
   return (
     <form>
       <input type="text" name="city" placeholder="都市" onChange={e => setCity(e.target.value)}/>
-      {city}
-      <button type="submit">Get Weather</button>
+      <button type="submit" onClick={getWeather}>Get Weather</button>
     </form> 
   );
 };
